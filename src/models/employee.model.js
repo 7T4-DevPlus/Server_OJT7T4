90bfc9608c92ac97b40e35fa11beea5b299f7f68
LuanVo <vothanhluan19102001@gmail.com>
2023-11-29 10:37:30 +0700

    create model base on ERD

---
+++ src/models/employee.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Technical = require('./technical.model');
+
+const Employee = new Schema({
+    name: String,
+    code: String,
+    phone: String,
+    email: String,
+    image: String,
+    identity: String,
+    gender: String,
+    isAvailable: Boolean,
+    isManager: Boolean,
+    isDelete: Boolean,
+    createAt: {
+        type: Date, 
+        default: Date.now
+    },
+    technical: {
+        type: Schema.Types.ObjectId, 
+        ref: Technical
+    },
+})
+
+module.exports = mongoose.model('Employee', Employee);
---
+++ src/models/employeeProject.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Employee = require('./employee.model');
+const Project = require('./project.model');
+const Role = require('./role.model');
+
+const EmployeeInProject = new Schema({
+    description: String,
+    joinDate: Date,
+    outDate: Date,
+    isWorking: Boolean,
+    role: {
+        type: Schema.Types.ObjectId, 
+        ref: Role
+    },
+    employeeId: {
+        type: Schema.Types.ObjectId, 
+        ref: Employee
+    },
+    projectId: {
+        type: Schema.Types.ObjectId, 
+        ref: Project
+    },
+})
+
+module.exports = mongoose.model('EmpPro', EmployeeInProject);
