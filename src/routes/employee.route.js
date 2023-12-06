ca501a357982a149baedb9bae12cd6862c5ea9a3
LuanVo <vothanhluan19102001@gmail.com>
2023-12-06 16:31:48 +0700

    create API for manage employee in project

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -4,11 +4,14 @@ const multer = require("multer");
 const upload = require("../utils/multer");
 
 const controller = require('../controllers/employee.controller');
+const employeeProjectController = require('../controllers/employeeProject.controller');
 
 router.get('/', controller.list);
 router.get('/:_id', controller.details);
 router.post('/create', upload.single("image"), controller.create);
 router.patch('/update/:_id', upload.single("image"), controller.update);
 router.patch('/delete/:_id', controller.delete);
+
+router.get('/history/:_id', employeeProjectController.getEmployeeHistory);
 
 module.exports = router;
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/project.controller');
+const employeeProjectController = require('../controllers/employeeProject.controller');
 
 router.get('/', controller.list);
 router.get('/:_id', controller.details);
 router.post('/create', controller.create);
 router.patch('/update/:_id', controller.update);
 router.patch('/close/:_id', controller.close);
+
+// router.get('/getEmployees/:_id', controller.getEmployeeInProject);
+// router.post('/addemp', controller.addEmployee);
+// router.patch('/removeemp/:_id', controller.removeEmployee);
+
+router.get('/getEmployees/:_id', employeeProjectController.getEmployeeInProject);
+router.post('/addemp', employeeProjectController.addEmployee);
+router.patch('/removeemp/:_id', employeeProjectController.removeEmployee);
 
 module.exports = router;
